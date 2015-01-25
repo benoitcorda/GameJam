f14e6bece9bea98f2f3dbdd61516b682236ad1c4
corda <corda@google.com>
2015-01-25 13:37:38 -0500

    add credits

--- Assets/Scripts/Fade.cs
+++ Assets/Scripts/Fade.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class Fade : MonoBehaviour {
 
 	public SpriteRenderer[] renderers;
+	public SpriteRenderer credits;
 
 	private float time = 0f;
 	private int idx = 0;
+	private bool isLoadingCredits;
 	private int[] scenrio1 = {0, 1, 2, 3 , 4};
 	private int[] scenrio2 = {0, 3, 2, 1};
 	private int[] scenrio3 = {3, 2, 3, 2};
@@ -24,16 +26,18 @@ public class Fade : MonoBehaviour {
 	void Start () {
 		// TODO(corda): Based on global context.
 		currentScenario = scenrio1;
+		isLoadingCredits = false;
 	}
 
 	void Update() {
 		var sceneLength = 5f;
-		var currentIdx = currentScenario[idx];
+		var currentIdx = idx < currentScenario.Length ? currentScenario[idx] : 0;
+		var currentSprite = isLoadingCredits ? credits : renderers [currentIdx];
 
 		// Set the colors on the previous and current scenes.
 		var interpolation = Mathf.Min(time/transitionTime, 1);
 		
-		renderers[currentIdx].color = Color.Lerp (hidden (), shown (), interpolation);
+		currentSprite.color = Color.Lerp (hidden (), shown (), interpolation);
 
 		// Switch to the next scene if needed.
 		time += Time.deltaTime;
@@ -43,15 +47,22 @@ public class Fade : MonoBehaviour {
 
 			// Did we show the last cutscene?
 			if (idx >= currentScenario.Length) {
-				// TODO(corda): Based on global context, go to the right level.
-				Application.LoadLevel(1);
+				LoadCredits(currentIdx);
 			} else {
 				// set the order of the layer to show up in front of the previous one
 				var newIdx = currentScenario[idx];
-				renderers[newIdx].sortingOrder = renderers[currentIdx].sortingOrder + 1;
+				renderers[newIdx].sortingOrder = currentSprite.sortingOrder + 1;
 			}
+		} else if (time > sceneLength && isLoadingCredits) {
+			// TODO(corda): Based on global context, go to the right level.
+			Application.LoadLevel(1);
 		}
-		
+	}
+
+	void LoadCredits(int lastSpriteIdx) {
+		isLoadingCredits = true;
+
+		credits.sortingOrder = renderers[lastSpriteIdx].sortingOrder + 1;
 
 	}
 
