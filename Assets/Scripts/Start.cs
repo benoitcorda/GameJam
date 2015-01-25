8024bdc5a1ec1e93c9987a71fb187a4f6ef9dddc
J. MacMillan <dvorakviolinist@gmail.com>
2015-01-25 14:59:11 -0500

    Update the start scene. Add a global state for level state.

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+
+	public void OnStartClicked() {
+		Application.LoadLevel((int)LevelState.levelState);
+	}
+}
